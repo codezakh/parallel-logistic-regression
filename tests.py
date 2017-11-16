ed58066a408ad28474627e1879c51d6c74a88d51
Zaid Khan <brain@zaidkhan.me>
2017-11-15 19:30:57 -0500

    Really bad testReally bad tests.

---
+++ tests.py
@@ -0,0 +1,34 @@
+import unittest
+
+import SparseVector
+import LogisticRegression
+
+
+
+class SparseVectorTestCase(unittest.TestCase):
+    def test_p_norm(self):
+        sparse = SparseVector.SparseVector({'a': 2, 'b': 2})
+
+class LogsticRegressionTestCase(unittest.TestCase):
+    def setUp(self):
+        self.x = SparseVector.SparseVector(dict(a=1,b=1))
+        self.y = 1
+        self.beta = SparseVector.SparseVector(dict(a=3,b=3))
+    def test_logisticLoss(self):
+        kek = LogisticRegression.logisticLoss(self.beta, self.x, self.y)
+    def test_gradLogisticLoss(self):
+        grad = LogisticRegression.gradLogisticLoss(self.beta, self.x, self.y)
+    def test_gradTotalLoss(self):
+        grad = LogisticRegression.gradTotalLoss([(self.x, self.y)], self.beta)
+    def test_test(self):
+        data = [
+        (SparseVector.SparseVector({'a': -1, 'b': -1}), -1),
+        (SparseVector.SparseVector({'a': -1, 'b': -1}), 1),
+        (SparseVector.SparseVector({'a':1, 'b': 1}), 1),
+        (SparseVector.SparseVector({'a':1, 'b': 1}), 1),
+        (SparseVector.SparseVector({'a':1, 'b': 1}), 1),
+        (SparseVector.SparseVector({'a':1, 'b': 1}), 1)
+        ]
+        beta = SparseVector.SparseVector({'a': 2, 'b': 2})
+        scores = LogisticRegression.test(data, beta)
+        print scores
